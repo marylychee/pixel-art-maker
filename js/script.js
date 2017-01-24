494bab4e321d5799935284cc8170f25690c3c0d6
Mary Lai <marylychee@gmail.com>
2017-01-24 14:17:01 -0800

    start on refactoring and fixing bugs that were highlighted by Hamid

--- js/script.js
+++ js/script.js
@@ -1,142 +1,127 @@
-
+// current color of the brush
 var currentColor = "black";
 
-
 // This creates the rows of pixels
-var pixels = document.getElementById('pixels');
 
 function createPixels() {
-
-  pixels.className = "newDiv row";
-
-  //creates multiple divs within the board, and sets the width, height and floats left
-
-  for (var i = 0; i <= 3093; i++) {
-      var innerDivs = document.createElement('div');
-      Object.assign(innerDivs.style, {float: "left", width: "10px", height: "10px", background: "white", border: "1px solid #e7e7e7"});
-
-      // appends divs to the parent board
-      pixels.appendChild(innerDivs);
+    let pixels = document.getElementById('pixels');
+    pixels.className = "newDiv row";
+    //creates multiple divs within the board, and sets the width, height and floats left
+    for (let i = 0; i <= 3093; i++) {
+        let innerDivs = document.createElement('div');
+        innerDivs.classList.add("pixel-squares");
+        // appends divs to the parent board
+        pixels.appendChild(innerDivs);
     }
-
-  return pixels;
-
+    return pixels;
 }
 
 createPixels();
 
+// add color responsive
+function init() {
+  let pixel = document.getElementById('pixels').children;
+  let painting = false;
 
-// // add color responsive
+  window.addEventListener('mousedown', function() {
+    painting = true;
 
-var pixel = pixels.children;
-
-var isMouseDown = false;
-
-window.addEventListener('mousedown', function () {
-  isMouseDown = true;
-})
-
-window.addEventListener('mouseup', function () {
-  isMouseDown = false;
-})
-
-function init () {
+  })
+  window.addEventListener('mouseup', function() {
+      painting = false;
+  })
 
   for (var k = 0; k < pixel.length; k++) {
-    pixel[k].addEventListener('mouseenter', function (){
-      if (isMouseDown) {
-        this.style.backgroundColor = currentColor;
-        this.style.border = currentColor;
-      }
-
-    });
-  }
-
+      pixel[k].addEventListener('mouseover', function() {
+          if (painting) {
+              this.style.backgroundColor = currentColor;
+              this.style.border = currentColor;
+          }
+      });
+      pixel[k].addEventListener('mousedown', function(e) {
+        e.style.backgroundColor = currentColor;
+        e.style.border = currentColor;
+        e.preventDefault();
+      });
+    }
 }
 
 init();
 
 
-
-
-
 // This creates the palette circles
 function createPalette() {
+    var palette = document.getElementById('palette');
 
-  var palette = document.getElementById('palette');
+    palette.className = "newDiv";
 
-  palette.className = "newDiv";
-
-  //creates multiple divs within the board, and sets the width, height and floats left
-  for (var i = 0; i < 20; i++) {
-    var innerDivs = document.createElement('div');
-    Object.assign(innerDivs.style, {float: "left", width: "3%", paddingBottom: "3%", background: "white", border: "1px solid #e7e7e7"});
-
-    // appends divs to the parent board
-    palette.appendChild(innerDivs);
-
-  }
-
-  return palette;
-
+    //creates multiple divs within the board, and sets the width, height and floats left
+    for (var i = 0; i < 20; i++) {
+        var innerDivs = document.createElement('div');
+        innerDivs.classList.add("palette-circles");
+        // appends divs to the parent board
+        palette.appendChild(innerDivs);
+    }
+    return palette;
 }
 
 createPalette();
 
 
-
-
 // COLOUR PALLETE
-
-//array for each for the colours
-let colorPalette = [
-  'rgb(210, 57, 57)',
-  'rgb(251, 107, 134)',
-  'rgb(255, 61, 114)',
-  'rgb(205, 34, 145)',
-  'rgb(189, 0, 255)',
-  'rgb(125, 46, 238)',
-  'rgb(0, 71, 255)',
-  'rgb(0, 67, 196)',
-  'rgb(36, 117, 224)',
-  'rgb(60, 215, 255)',
-  'rgb(0, 255, 201)',
-  'rgb(44, 238, 167)',
-  'rgb(176, 254, 76)',
-  'rgb(255, 252, 0)',
-  'rgb(255, 253, 101)',
-  'rgb(255, 203, 18)',
-  'rgb(255, 171, 60)',
-  'rgb(255, 122, 0)',
-  'rgb(255, 255, 255)',
-  'rgb(38, 38, 38)'
-];
-
-
-
-
-// creates the array of palette circles
-
-var paletteArr = document.getElementById("palette").children;
-console.log(paletteArr);
-
-
+// creates the array of palette circles and thier colors
 function initColor() {
+  let paletteArr = document.getElementById("palette").children;
 
-    for (let j = 0; j < paletteArr.length; j++) {
-        Object.assign(paletteArr[j].style, { backgroundColor : colorPalette[j] });
-        paletteArr[j].addEventListener('click', function () {
+  //array for each for the colours
+  let colorPalette = [
+      'rgb(210, 57, 57)',
+      'rgb(251, 107, 134)',
+      'rgb(255, 61, 114)',
+      'rgb(205, 34, 145)',
+      'rgb(189, 0, 255)',
+      'rgb(125, 46, 238)',
+      'rgb(0, 71, 255)',
+      'rgb(0, 67, 196)',
+      'rgb(36, 117, 224)',
+      'rgb(60, 215, 255)',
+      'rgb(0, 255, 201)',
+      'rgb(44, 238, 167)',
+      'rgb(176, 254, 76)',
+      'rgb(255, 252, 0)',
+      'rgb(255, 253, 101)',
+      'rgb(255, 203, 18)',
+      'rgb(255, 171, 60)',
+      'rgb(255, 122, 0)',
+      'rgb(255, 255, 255)',
+      'rgb(38, 38, 38)'
+  ];
+
+  for (let j = 0; j < paletteArr.length; j++) {
+      Object.assign(paletteArr[j].style, {
+          backgroundColor: colorPalette[j]
+      });
+      paletteArr[j].addEventListener('click', function() {
           //This is variable holding the rgb value
           currentColor = colorPalette[j];
-
 
           //this sets the brush to the current color
           let brushie = document.getElementById('brushie');
           brushie.style.fill = currentColor;
+      });
+  }
 
-        });
-    }
-    return palette;
-
+  return palette;
 }
+
 initColor();
+
+
+//Corrections to ammend still Hamid's advice...
+
+// Paint on click  - done
+// Get rid of white space - done
+// Drag and Drop css - done
+// Fastdrag Paint function - follow up on this.
+// Reduce use of global variables and use let and const instead
+// Style elements inside css, alot easier to read.
